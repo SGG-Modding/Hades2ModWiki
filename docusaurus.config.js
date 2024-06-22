341f39e442211950bec18e995b88a47893dc8e1f
Jen Guerra <southpawgeek@users.noreply.github.com>
2024-06-22 12:18:11 -0400

    remove blog link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -118,10 +118,10 @@ const config = {
           {
             title: "More",
             items: [
-              {
-                label: "Blog",
-                to: "/blog",
-              },
+              // {
+              //   label: "Blog",
+              //   to: "/blog",
+              // },
               {
                 label: "GitHub",
                 href: "https://github.com/sgg-modding",
