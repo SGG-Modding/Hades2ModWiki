9e7e6ff2e0839393c44a16dc9fff3e56c99a638c
Jen Guerra <southpawgeek@users.noreply.github.com>
2024-06-22 12:14:28 -0400

    disable blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -48,13 +48,14 @@ const config = {
           editUrl:
             "https://github.com/sgg-modding/hades2modwiki/tree/main/packages/create-docusaurus/templates/shared/",
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/sgg-modding/hades2modwiki/tree/main/packages/create-docusaurus/templates/shared/",
-        },
+        // blog: {
+        //   showReadingTime: true,
+        //   // Please change this to your repo.
+        //   // Remove this to remove the "edit this page" links.
+        //   editUrl:
+        //     "https://github.com/sgg-modding/hades2modwiki/tree/main/packages/create-docusaurus/templates/shared/",
+        // },
+        blog: false,
         theme: {
           customCss: "./src/css/custom.css",
         },
@@ -85,7 +86,7 @@ const config = {
             position: "left",
             label: "Tutorial",
           },
-          { to: "/blog", label: "Blog", position: "left" },
+          //{ to: "/blog", label: "Blog", position: "left" },
           {
             href: "https://github.com/sgg-modding/hades2modwiki",
             label: "GitHub",
